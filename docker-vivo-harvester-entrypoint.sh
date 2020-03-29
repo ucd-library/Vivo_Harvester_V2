eccb9e5bb9fd066637940937a12a974e46bc43df
Quinn Hart <qjhart@ucdavis.edu>
2020-03-29 02:22:07 -0700

    Added state.txt to initialization.

--- docker-vivo-harvester-entrypoint.sh
+++ docker-vivo-harvester-entrypoint.sh
@@ -3,18 +3,24 @@
 d=/usr/local/vivo/harvester
 tar=~/elements-vivo-harvester-${VIVO_HARVESTER_VERSION}.tar.gz
 
-if [[ ! -f ${d}/elementsfetch.properties ]] ; then
-  cd ${d};
-  tar -xzf ${tar}
-  echo untarring ${tar} and copying examples to ${d}
-  cp examples/example-config/* .
-  cp examples/example-bin/*.sh .
+function initialize_state_txt() {
+  echo '0'
+  date --utc --iso-8601=sec
+  echo '0'
+}
+
+# Don't run if a state.txt file exists
+if [[ ! -f ${d}/state.txt ]] ; then
+  echo "Initializing ${d}"
+  tar -xzf --directory=${d} ${tar}
+  cp examples/example-config/* ${d}
+  cp examples/example-bin/*.sh ${d}
   mkdir scripts && cp -r examples/example-scripts/example-elements scripts
+  initialize_state_txt > ${d}/state.txt
+  mkdir ${d}/data
+else
+  echo "${d}/state.txt exists, previously initialized"
 fi
 
-# Need to just copy the properties files to /etc/vivo/harvester/ ?
-# or maybe root?
-
-
-# Is this really the best method?
+# Switch to CMD
 exec "$@"
