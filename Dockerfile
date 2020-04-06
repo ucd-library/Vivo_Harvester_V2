f8e01ab2886cd9a60c02dd12015aeec1c372fc4b
Quinn Hart <qjhart@ucdavis.edu>
2020-04-06 08:30:56 -0700

    Updated local XSL.

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,14 @@ ENV VIVO_HARVESTER_VERSION=1.1.0
 
 RUN git clone $repo && cd Vivo_Harvester_V2/elements-harvester && mvn install
 RUN mkdir -p /usr/local/vivo/harvester-example && cd /usr/local/vivo/harvester-example \
- && cp ~/.m2/repository/uk/co/symplectic/elements-vivo-harvester/1.1.0/elements-vivo-harvester-1.1.0.tar.gz ~
+ && cp ~/.m2/repository/uk/co/symplectic/elements-vivo-harvester/1.1.0/elements-vivo-harvester-1.1.0.tar.gz /root
 
+ARG d=/usr/local/vivo/harvester
+
+COPY config/scripts $d/scripts
+RUN tar -xz --file=/root/elements-vivo-harvester-1.1.0.tar.gz --directory=$d lib examples && cp $d/examples/example-bin/*.sh $d
 COPY docker-vivo-harvester-entrypoint.sh /usr/local/bin/
+COPY config /root/config
 
 ENTRYPOINT ["/usr/local/bin/docker-vivo-harvester-entrypoint.sh"]
 CMD ["/bin/echo", "VIVO Harvester @ /usr/local/vivo/harvester"]
--- docker-vivo-harvester-entrypoint.sh
+++ docker-vivo-harvester-entrypoint.sh
@@ -5,19 +5,21 @@ tar=~/elements-vivo-harvester-${VIVO_HARVESTER_VERSION}.tar.gz
 
 function initialize_state_txt() {
   echo '0'
-  date --utc --iso-8601=sec
+  date --utc --iso-8601=sec | sed -e 's/:00/00/'
   echo '0'
 }
 
+# If you haven't copied the volume, than this will
 # Don't run if a state.txt file exists
 if [[ ! -f ${d}/state.txt ]] ; then
   echo "Initializing ${d}"
-  tar -xzf --directory=${d} ${tar}
-  cp examples/example-config/* ${d}
-  cp examples/example-bin/*.sh ${d}
-  mkdir scripts && cp -r examples/example-scripts/example-elements scripts
-  initialize_state_txt > ${d}/state.txt
-  mkdir ${d}/data
+  for i in /root/config/*.properties; do
+      n=$(basename $i)
+      sed -e 's|\${env:VIVO_ROOT_PASSWORD}|'"$VIVO_ROOT_PASSWORD"'|g' <$i >${d}/$n
+  done
+  initialize_state_txt > ${d}/data/state.txt
+  ln -s ${d}/data/state.txt ${d}/state.txt
+#  [[ -d ${d}/data/other_data ]] || mkdir ${d}/data/other_data
 else
   echo "${d}/state.txt exists, previously initialized"
 fi
